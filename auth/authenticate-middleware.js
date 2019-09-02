07e9ad203064e7155a7543992d18ff8ed1737d15
Ihab Ali <ihab@live.com>
2019-09-02 02:26:09 -0500

    added auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,28 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const secret = secrets.jwtSecret;
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({
+          message: "Incorrect Username or Password. Please try again!"
+        });
+      } else {
+        req.user = {
+          username: decodedToken.uusername
+        };
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No token provided, Log In first!" });
+  }
 };
