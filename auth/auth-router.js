2f476c29684779a5189f1a9dd3398616631e1753
Ihab Ali <ihab@live.com>
2019-09-02 03:13:21 -0500

    debug some errors

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,41 +1,47 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const Users = require("../users/users-model");
-const secrets = require("../config/secrets.js");
 const jwt = require("jsonwebtoken");
+
+const UsersDb = require("../users/users-model");
+const secrets = require("../config/secrets.js");
 
 router.post("/register", (req, res) => {
   // implement registration
-  const hash = bcrypt.hashSync(req.body.password, 10);
-  req.body.password = hash;
+  let user = req.body;
 
-  Users.add(req.body)
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  UsersDb.add(user)
     .then(saved => {
       res.status(201).json(saved);
     })
-    .catch(err => {
-      res.status(500).json(err);
+    .catch(error => {
+      res.status(500).json(error);
     });
 });
 
 router.post("/login", (req, res) => {
   // implement login
   let { username, password } = req.body;
-  Users.findBy({ username })
+
+  UsersDb.findBy({ username })
+    .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = genToken(user);
-        res.status(200).json({ message: `Welcome ${username}`, token });
+        const token = generateToken(user);
+
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
     })
-    .catch(err => {
-      res.status(500).json({ err: err.message });
+    .catch(error => {
+      res.status(500).json({ message: "Error logging in!" });
     });
 });
 
-function genToken(user) {
+function generateToken(user) {
   const payload = {
     subject: "users",
     username: user.username
