948abfc12164c41bb15037c6e25f5673046ed35a
Ihab Ali <ihab@live.com>
2019-09-02 02:10:52 -0500

    added login and registration endpoints and encrypt the password with bcryptjs

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("../users/users-model");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+  const hash = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hash;
+
+  Users.add(req.body)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${username}` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err: err.message });
+    });
 });
 
 module.exports = router;
